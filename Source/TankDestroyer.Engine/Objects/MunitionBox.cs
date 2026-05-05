f04fa8785b6ac2b4e98a96d19f665aab9a8f463e
Liam Omen <liam.sp@hotmail.com>
2026-05-05 13:32:19 +0200

    Constructor

--- Source/TankDestroyer.Engine/Objects/MunitionBox.cs
+++ Source/TankDestroyer.Engine/Objects/MunitionBox.cs
@@ -7,4 +7,11 @@ public class MunitionBox : IMunitionBox
     public int Amount { get; set; }
     public int X { get; set; }
     public int Y { get; set; }
+
+    public MunitionBox(int x, int y, int amount = 10)
+    {
+        Amount = amount;
+        X = x;
+        Y = y;
+    }
 }
